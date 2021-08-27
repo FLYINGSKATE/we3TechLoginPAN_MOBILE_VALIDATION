c1f112601f45aa29dcfac80a52540754e438b617
Khan Ashraf Salim <ASHRAFKHAN84@OUTLOOK.COM>
2021-08-27 05:31:26 -0700

    Update nuniyo_bank_email_pan_validation_screen.dart

--- lib/screens/nuniyo_bank_email_pan_validation_screen.dart
+++ lib/screens/nuniyo_bank_email_pan_validation_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
+/////Bank
 class BankPanEmailValidationScreen extends StatefulWidget {
   const BankPanEmailValidationScreen({Key? key}) : super(key: key);
 
